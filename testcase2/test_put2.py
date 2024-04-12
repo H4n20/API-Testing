23f9021f1ddafe242a680c24074856580ddc63fc
H4n20 <165984049+H4n20@users.noreply.github.com>
2024-04-12 13:32:56 +0700

    Create  test_put2.py

---
+++ testcase2/test_put2.py
@@ -0,0 +1,62 @@
+import requests
+import json
+import string
+import random
+
+
+base_url = "https://gorest.co.in"
+auth_token = "Bearer e4b8e1f593dc4a731a153c5ec8cc9b8bbb583ae964ce650a741113091b4e2ac6"
+
+def create_random_email():
+    domain = "automation.com"
+    email_length = 10
+    random_string = ''.join(random.choice(string.ascii_lowercase) for _ in range (email_length))
+    email = random_string + "@" + domain
+    return email
+
+def post_request():
+    url = base_url + "/public/v2/users"
+    print("post url: " + url)
+    headers = {"Authorization": auth_token}
+    new_user = {
+        "name": "Naveen Automation",
+        "email": create_random_email(),
+        "gender": "male",
+        "status": "active"
+    }
+    response = requests.post(url, json=new_user, headers=headers)
+    json_data = response.json()
+    print("Status code:" , response.status_code)
+    json_str = json.dumps(json_data, indent=4)
+    print("json POST response body: ", json_str)
+    user_id = json_data["id"]
+    print("user id ===>", user_id)
+    assert response.status_code == 201
+    assert "name" in json_data
+    assert json_data["name"] == "Naveen Automation"
+    print(".......POST/Create USER IS DONE.......")
+    print(".......=====================.......")
+    return user_id
+
+def put_request(user_id):
+    url = base_url + f"/public/v2/users/{user_id}"
+    print("PUT url: " + url)
+    data = {
+        "name": "Naveen Automation Labs",
+        "email": create_random_email(),
+        "gender": "male",
+        "status": "inactive"
+    }
+    response = requests.put(url, json=data)
+    print("Status code:" , response.status_code)
+    assert response.status_code == 200
+    json_data = response.json()
+    json_str = json.dumps(json_data, indent=4)
+    print("json PUT response body: ", json_str)
+    assert json_data["id"] == user_id
+    assert json_data["name"] == "Naveen Automation Labs"
+    print(".......PUT/Update USER IS DONE.......")
+    print(".......=====================.......")
+
+user_id = post_request()
+put_request(user_id)
